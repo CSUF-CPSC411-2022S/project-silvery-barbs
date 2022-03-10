a1b7b331e4a7e5e9d5e6f382f2db673c53244370
Kierstyn Just <kjust@csu.fullerton.edu>
2022-03-09 18:19:13 -0800

    Set up testing file but haven't added tests yet

---
+++ Dnd-Planner/Dnd-PlannerUITests/MapManagerUITests.swift
@@ -0,0 +1,14 @@
+//
+//  MapManagerUITests.swift
+//  Dnd-PlannerUITests
+//
+//  Created by Kierstyn Just on 3/8/22.
+//
+
+import XCTest
+
+class MapManagerUITests: XCTestCase {
+    // mile radius over edge
+    // mile radius under edge
+    // mile radius negative
+}

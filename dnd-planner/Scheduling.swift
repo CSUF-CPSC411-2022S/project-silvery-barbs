7a5421697fb7c41de1dfd11e51411a02aa3a1cda
Minkyu Park <pmk0625@hotmail.com>
2022-03-10 13:34:17 -0800

    Second Commit

---
+++ dnd-planner/Scheduling.swift
@@ -0,0 +1,18 @@
+//
+//  Scheduling.swift
+//  Dnd-Planner
+//
+//  Created by Minkyu Park on 2/25/22.
+//
+
+import SwiftUI
+
+/*
+
+struct Scheduling: AppDelegate {
+    var body: some Scene {
+        WindowGroup {
+            SchedulingView()
+        }
+    }
+}*/

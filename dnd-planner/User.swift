d433dd1eac69904dffda98fd54e49a36c5db9823
Benjamin Ahn <benjaminsolbiahn@gmail.com>
2022-03-24 14:34:44 -0700

    new branch

---
+++ dnd-planner/User.swift
@@ -0,0 +1,86 @@
+//
+//  User.swift
+//  Dnd-Planner
+//
+//  Created by csuftitan on 2/24/22.
+//
+
+import Foundation
+
+class User {
+    var username: String
+    //how do you hash passwords and create a secure database???
+    var password: String
+    var email: String?
+    var phonenumber: String?
+    
+    var DM_status: Bool
+    var group_status: Bool
+    var verified: Bool
+    //TODO
+    //how do you find a user location??? need a var for that
+    
+    //full user account
+    init(username: String, password: String, email: String, phonenumber: String, DM_status: Bool, group_status: Bool){
+        self.username = username
+        self.password = password
+        self.email = email
+        self.phonenumber = phonenumber
+        
+        self.DM_status = DM_status
+        self.group_status = group_status
+        
+        self.verified = false
+    }
+    
+    //user sans email
+    init(username: String, password: String, phonenumber: String, DM_status: Bool, group_status: Bool){
+        self.username = username
+        self.password = password
+        self.email = nil
+        self.phonenumber = phonenumber
+        
+        self.DM_status = DM_status
+        self.group_status = group_status
+        
+        self.verified = false
+    }
+    
+    //user sans phone number
+    init(username: String, password: String, email: String, DM_status: Bool, group_status: Bool){
+        self.username = username
+        self.password = password
+        self.email = email
+        self.phonenumber = nil
+        
+        self.DM_status = DM_status
+        self.group_status = group_status
+        
+        self.verified = false
+    }
+    
+    //user sans any verification
+    init(username: String, password: String, DM_status: Bool, group_status: Bool){
+        self.username = username
+        self.password = password
+        self.email = nil
+        self.phonenumber = nil
+        
+        self.DM_status = DM_status
+        self.group_status = group_status
+        
+        self.verified = false
+    }
+    
+    //TODO
+    //create a function to verify users
+    //create functions to add emails and phone numbers to already created accounts
+    
+    func set_user_DM_status(){
+        //TODO UI is set up, how to link it to content view?
+    }
+    
+    func set_user_group_status(){
+        //TODO UI is set up, how to link it to content view?
+    }
+}
---
+++ dnd-planner/UserView.swift
@@ -0,0 +1,128 @@
+//
+//  UserView.swift
+//  Dnd-PlannerTests
+//
+//  Created by csuftitan on 3/8/22.
+//
+
+import SwiftUI
+//import User how do we import user file?
+
+
+struct UserView: View {
+    @State var username: String = ""
+    @State var password: String = ""
+    @State var email: String = ""
+    @State var phonenumber: String = ""
+    
+    @State var DM_status = false
+    @State var group_status = false
+    
+    //TODO need to figure out how to import the user file in order to do this
+    
+    var body: some View {
+        //banner
+        VStack {
+            Text("User Account Creation")
+                .font(.custom("Courier New", size: 30))
+                .foregroundColor(Color.white)
+                .padding(10)
+                .background(Color.black)
+                .cornerRadius(10)
+            //user info
+            VStack {
+                Text("Name: ")
+                    .font(.custom("Courier New", size: 20))
+                    .foregroundColor(Color.white)
+                    .padding(10)
+                    .background(Color.black)
+                    .cornerRadius(10)
+                TextField("Name", text: $username)
+                    .font(.custom("Arial", size: 20))
+                    .frame(width: 250, alignment: .trailing)
+            }
+            VStack {
+                Text("Password: ")
+                    .font(.custom("Courier New", size: 20))
+                    .foregroundColor(Color.white)
+                    .padding(10)
+                    .background(Color.black)
+                    .cornerRadius(10)
+                TextField("Password", text: $password)
+                    .font(.custom("Arial", size: 20))
+                    .frame(width: 250, alignment: .trailing)
+            }
+            VStack {
+                Text("Email: ")
+                    .font(.custom("Courier New", size: 20))
+                    .foregroundColor(Color.white)
+                    .padding(10)
+                    .background(Color.black)
+                    .cornerRadius(10)
+                TextField("Email", text: $email)
+                    .font(.custom("Arial", size: 20))
+                    .frame(width: 250, alignment: .trailing)
+            }
+            VStack {
+                Text("Phone Number: ")
+                    .font(.custom("Courier New", size: 20))
+                    .foregroundColor(Color.white)
+                    .padding(10)
+                    .background(Color.black)
+                    .cornerRadius(10)
+                TextField("Phone Number", text: $phonenumber)
+                    .font(.custom("Arial", size: 20))
+                    .frame(width: 250, alignment: .trailing)
+            }
+            VStack {
+                Text("Looking to DM? ")
+                    .foregroundColor(Color.white)
+                    .padding(10)
+                    .background(Color.black)
+                    .cornerRadius(10)
+                
+                    Toggle("Looking to DM?", isOn: $DM_status)
+                        .font(.custom("Courier New", size: 20))
+                        .frame(width: 250, alignment: .trailing)
+                
+            }
+            VStack {
+                Text("Looking for a group? ")
+                    .foregroundColor(Color.white)
+                    .padding(10)
+                    .background(Color.black)
+                    .cornerRadius(10)
+                
+                    Toggle("Looking for a group?", isOn: $group_status)
+                        .font(.custom("Courier New", size: 20))
+                        .frame(width: 250, alignment: .trailing)
+            }.padding()
+        }
+    }
+}
+
+struct Information: View {
+    @Binding var username: String
+    @Binding var password: String
+    @Binding var email: String
+    @Binding var phonenumber: String
+    
+    @Binding var DM_status: Bool
+    @Binding var group_status: Bool
+    
+    var body: some View {
+        Text(username)
+        Text(password)
+        Text(email)
+        Text(phonenumber)
+        
+        //Text(DM_status)
+        //Text(group_status)
+    }
+}
+    
+struct UserView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

725d81fd061cf9ee9ddd1adcb09864eded51cf6b
Kierstyn Just <kjust@csu.fullerton.edu>
2022-04-06 17:50:12 -0700

    Added settings page & dynamic data display

--- dnd-planner/NearbyUsersView.swift
+++ dnd-planner/NearbyUsersView.swift
@@ -7,54 +7,92 @@
 
 import SwiftUI
 
-// TO ADD
-// - button or tetfield to change location
-// - something to change mile radius (maybe slider or spinner)
-// - NavigationTitle or dropdowm to separate users looking to join a party
-//    or looking for a user to join their party
-
 struct NearbyUsersView: View {
-    var manager = MapManager()
+    @StateObject var manager = MapManager()
+    
     var body: some View {
         GeometryReader { geometry in
-            VStack {
-                Text("Fullerton, CA")
-                Text("Nearby Users")
-                    .font(.headline)
-                List {
-                    ForEach(manager.nearbyUsers) { user in
-                        HStack {
-                            Text(user.username)
-                                .frame(width: geometry.size.width / 4, alignment: Alignment.leading)
-                            Text(String(user.milesAway) + " Miles Away")
-                                .font(.custom("System", size: 14))
-                                .frame(width: geometry.size.width / 4, alignment: Alignment.trailing)
+            NavigationView {
+                VStack { // entire view
+                    VStack {
+                        HStack { // header bar
+                            VStack {
+                                Text("\(manager.currentLocation)").bold()
+                                Text("Mile Radius: \(Int(manager.mileRadius))")
+                            }
+                            Spacer()
+                            NavigationLink(destination: NearbyUsersSettingsView()) {
+                                Text("Filter")
+                            }
+                        }
+                    }.padding()
+                    List {
+                        ForEach(manager.getFilteredUsers()) { user in
                             HStack {
-                                Button(action: {}) {
-                                    Text("Add").modifier(UserButtonText())
-                                }.modifier(UserButtons())
-                                Button(action: {}) {
-                                    Text("✉️").modifier(UserButtonText())
-                                }.modifier(UserButtons())
-                            }.frame(width: geometry.size.width / 3)
+                                Text(user.username)
+                                    .frame(width: geometry.size.width / 4, alignment: Alignment.leading)
+                                Text(String(Int(user.milesAway)) + " Miles Away")
+                                    .font(.custom("System", size: 14))
+                                    .frame(width: geometry.size.width / 4, alignment: Alignment.trailing)
+                                HStack {
+                                    Button(action: {}) {
+                                        Image(systemName: "plus").modifier(UserButtonText())
+                                    }.modifier(UserButtons())
+                                    Button(action: {}) {
+                                        Image(systemName: "envelope").modifier(UserButtonText())
+                                    }.modifier(UserButtons())
+                                }.frame(width: geometry.size.width / 3)
+                            }
                         }
                     }
-                }.frame(width: geometry.size.width, height: geometry.size.height / 2)
-            }.frame(width: geometry.size.width)
-        }
+                }.frame(height: geometry.size.height)
+            }
+        }.environmentObject(manager)
     }
 }
 
 struct NearbyUsersView_Previews: PreviewProvider {
     static var previews: some View {
         NearbyUsersView()
+        NearbyUsersSettingsView().environmentObject(MapManager())
     }
 }
 
+struct NearbyUsersSettingsView: View {
+    @EnvironmentObject var manager: MapManager
+    
+    var body: some View {
+        VStack {
+            Text("Settings")
+                .font(.largeTitle)
+            List {
+                Section(header: Text("Set Radius")) {
+                    VStack {
+                        Slider(value: $manager.mileRadius, in: 0...50, step: 5)
+                        {
+                            
+                        } minimumValueLabel: {
+                            Text("0")
+                        } maximumValueLabel: {
+                            Text("50")
+                        }
+                        Text("\(Int(manager.mileRadius)) Miles Away")
+                    }
+                }
+                Section(header: Text("Search Type")) {
+                    Toggle("Looking for Party Members", isOn: $manager.lookingForPartyMembers)
+                    Toggle("Looking to Join Party", isOn: $manager.lookingToJoinParty)
+                }
+            }
+        }
+    }
+}
+
+// ===== VIEW MODIFIERS =====
 struct UserButtons: ViewModifier {
     func body(content: Content) -> some View {
         content
-            .background(Color.accentColor)
+            .background(Color.gray)
             .foregroundColor(Color.white)
             .cornerRadius(5)
     }
